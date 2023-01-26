207fe3186d44f95ef7693b20d919ff9ff37424c5
Caio Espindola <caioespindola@hotmail.com>
2023-01-26 10:38:29 -0300

    More Studies

---
+++ ternario.js
@@ -0,0 +1,16 @@
+// Operador Ternário. Compara e retorna True or False.
+
+// Método if.
+
+const minAge = 18
+const clientAge = 16
+
+if (clientAge >= minAge) {
+  console.log('Beer')
+} else {
+  console.log('Juice')
+}
+
+//Outro método, mas não é muito recomendado.
+
+console.log(clientAge >= minAge ? 'Cerveja' : 'Suco')
